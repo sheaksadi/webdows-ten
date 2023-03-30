0986a3b633bebb0f377321e2114609ab2a4fde78
sheaksadi <sheaksadi123456@gmail.com>
2023-03-30 12:53:44 +0600

    mobile embed test

--- components/windows/window-rickRoll.vue
+++ components/windows/window-rickRoll.vue
@@ -1,11 +1,21 @@
 <script setup>
+import { webStore } from "/stores/webStore.js";
 
+const store = webStore();
 import Window from "../window";
+
+
+const getLink = computed(() => {
+  return store.isDeviceMobile ? "http://www.youtube.com/embed/o-YBDTqX_ZU" : "http://www.youtube.com/embed/o-YBDTqX_ZU?autoplay=1";
+});
+
+
+
 </script>
 <template>
   <Window icon="mdi:warning" title="LOL">
     <div class="h-full w-full bg-slate-900" >
-      <iframe class="h-full w-full" src="http://www.youtube.com/embed/o-YBDTqX_ZU?autoplay=1" ></iframe>
+      <iframe class="h-full w-full" :src="getLink" ></iframe>
     </div>
 
   </Window>
