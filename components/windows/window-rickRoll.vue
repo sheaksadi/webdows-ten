41b14003179fab970c0bfce5e237b08dda96f7c3
sheaksadi <sheaksadi123456@gmail.com>
2023-04-01 10:28:39 +0600

    lol test 2

--- components/windows/window-rickRoll.vue
+++ components/windows/window-rickRoll.vue
@@ -13,10 +13,7 @@ const getLick = computed(() => {
 <template>
   <Window icon="mdi:warning" title="LOL">
     <div class="h-full w-full bg-slate-900" >
-      <iframe class="h-full w-full"
-              src="https://www.youtube.com/embed/o-YBDTqX_ZU">
-      </iframe>
-
+      <iframe class="h-full w-full" src="http://www.youtube.com/embed/o-YBDTqX_ZU?autoplay=1" allow="autoplay; encrypted-media"></iframe>
     </div>
 
   </Window>
